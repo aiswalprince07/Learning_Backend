3d21cc3102f71b138cac9fe950b257c8a7e4b153
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2023-06-11 11:38:09 +0530

    create server by using express and Learn run API's from Postman

--- index.js
+++ index.js
@@ -1,6 +1,80 @@
+//Express :-
+const fs = require('fs');
+const index = fs.readFileSync('index.html','utf-8');
+const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
+const products = data.products;
+
+const express = require('express');
+const server = express();
+
+
+//API's
+
+server.get('/',(req,res)=>{
+    res.json({type:"GET"})
+})
+server.post('/',(req,res)=>{
+    res.json({type:"POST"})
+})
+server.put('/',(req,res)=>{
+    res.json({type:"PUT"})
+})
+server.delete('/',(req,res)=>{
+    res.json({type:"DELETE"})
+})
+server.patch('/',(req,res)=>{
+    res.json({type:"PATCH"})
+})
+
+
+server.listen(5000,()=>{
+    console.log("server started");
+});
+
+
+
+
+
+
+/* --> Creating server with the help of express :-
+
+const express = require('express');
+
+const server = express();   // server <==>app
+server.listen(5000,()=>{
+    console.log("server started");
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Learning http ,fs module :
 //Read file and dynamically change the content like -->title ,price,id from JSON data
 
+/*
 const fs = require('fs');
 const http = require('http');
 
@@ -32,7 +106,7 @@ const server = http.createServer((req,res)=>{
 server.listen(5000);
 
 
-
+*/
 
 
 /*
